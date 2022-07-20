b5596e4ef21f3813c529118ea61ba8b30d2ca3df
Sean-Long-ATL <47513921+Sean-Long-ATL@users.noreply.github.com>
2022-07-20 16:20:13 -0400

    update win condition to text update

--- puzzle.js
+++ puzzle.js
@@ -1,26 +1,11 @@
 let puzzle = document.getElementById('puzzle');
 let isSolved = 0;
-
 createBoard();
-// shuffle();
+//shuffle();
 
 puzzle.addEventListener("click", function(e) {
     move(e.target);
   });
-
-puzzle.addEventListener("mouseover", function(e) {
-         if (getEmptyNeighbor(getNeighbors(e.target)) !== -1) {
-            e.target.classList.remove("tile");
-            e.target.classList.add("movableTile");
-         }
-    });
-
-puzzle.addEventListener("mouseout", function(e) {
-    if (e.target.classList.contains("movableTile")) {
-        e.target.classList.remove("movableTile");
-        e.target.classList.add("tile");
-    }
-});
 
 
 //create and populate 4x4 board 
@@ -29,14 +14,16 @@ function createBoard() {
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
             let tile = document.createElement('div');
-            tile.id = "tile_" + i + "_" + j;
-
+       
             tile.style.backgroundPosition = '-' + j*100 + 'px' + " " +'-' + i*100 + "px" ; //this math is off 
             tile.style.backgroundImage = "url(jwst.jpg)";
             if(j==3 && i==3){
                 tile.style.backgroundImage="url(white.jpg)";
+                console.log("test")
             }
+       
 
+            tile.id = "tile_" + i + "_" + j;
             tile.classList.add('tile');
             count++;
             tile.innerHTML = count.toString();
@@ -59,7 +46,9 @@ function shuffle() {
         rand = parseInt(Math.random() * (neighbors.length));
         move(neighbors[rand]);
     }
-
+    let text = document.getElementById("win");
+    text.innerHTML= "";
+    // // Init/start timer and music
     initializeTimer();
     setInterval(showInterval, 1000);
     setTimeout(play, 500);
@@ -115,45 +104,38 @@ function getEmptyNeighbor(neighbors) {
 
 //check if a tile is moveable and add class 'moveableTile' if it is
 function checkMoveable(targetTile) {
-    if (targetTile.classList.contains("movableTile")) {
-        targetTile.classList.remove("movableTile");
-        targetTile.classList.add("tile");
+    if (getEmptyNeighbor(getNeighbors(targetTile)) === -1) {
+        targetTile.classList.add('moveableTile');
+    }
+    else {
+        return -1;
     }
 }
 
 //moves target tile to empty neighbor if one exists
 function move(targetTile) {
-        let emptyTile = getEmptyNeighbor(getNeighbors(targetTile));
+    let emptyTile = getEmptyNeighbor(getNeighbors(targetTile));
 
-        if (emptyTile === -1) {
-            return;
-        }
-
-        //swap target tile and empty tile
-        let tempTile = { className: targetTile.className, id: targetTile.id, innerHTML: targetTile.innerHTML, backgroundImage: targetTile.style.backgroundImage, backgroundPosition: targetTile.style.backgroundPosition};
-
-        targetTile.className = emptyTile.className;
-        targetTile.innerHTML = emptyTile.innerHTML;
-        targetTile.style.backgroundPosition = emptyTile.style.backgroundPosition;
-        targetTile.style.backgroundImage = emptyTile.style.backgroundImage;//here
-        emptyTile.className = tempTile.className;
-        emptyTile.innerHTML = tempTile.innerHTML;
-        emptyTile.style.backgroundPosition = tempTile.backgroundPosition;
-        emptyTile.style.backgroundImage = tempTile.backgroundImage;//here
-
-        checkSolved();
-
-        let count = 0;
-        let currentTile;
-
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; j < 4; j++) {
-                count++;
-                currentTile = getTile(i, j);
-                checkMoveable(currentTile);
-        }
+    if (emptyTile === -1) {
+        return;
     }
+
+    //swap target tile and empty tile
+    let tempTile = { className: targetTile.className, id: targetTile.id, innerHTML: targetTile.innerHTML, backgroundImage: targetTile.style.backgroundImage, backgroundPosition: targetTile.style.backgroundPosition};
+
+    targetTile.style.backgroundPosition = emptyTile.style.backgroundPosition;
+    targetTile.style.backgroundImage = emptyTile.style.backgroundImage;//here
+    targetTile.className = emptyTile.className;
+    targetTile.innerHTML = emptyTile.innerHTML;
+
+    emptyTile.style.backgroundPosition = tempTile.backgroundPosition;
+    emptyTile.style.backgroundImage = tempTile.backgroundImage;//here
+    emptyTile.className = tempTile.className;
+    emptyTile.innerHTML = tempTile.innerHTML;
+
+    checkSolved();
 }
+
 
 function checkSolved() {
     let count = 0;
@@ -163,6 +145,7 @@ function checkSolved() {
         for (var j = 0; j < 4; j++) {
             count++;
             currentTile = getTile(i, j);
+            //console.log(count + ", " + currentTile.innerHTML);
             if (currentTile.className != "emptyTile") {
                 if (currentTile.innerHTML != count) {
                     return;
@@ -171,12 +154,13 @@ function checkSolved() {
         }
     }
     isSolved = 1;
-    alert("You Won!");
+    let text = document.getElementById("win");
+    text.innerHTML= "You Won!!!";   
     return 1;
 }
 
 function changeBackgroundImage (image) {
-
+ 
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
             currentTile = getTile(i, j);
@@ -187,5 +171,3 @@ function changeBackgroundImage (image) {
             }
         }
     } 
-
-
