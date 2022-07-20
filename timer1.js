08c71e52154083019fb758a8dfd1e4def6cc7ac8
Radagast97 <107087358+Radagast97@users.noreply.github.com>
2022-07-20 14:20:44 -0400

    Correct an erroneous change

--- timer1.js
+++ timer1.js
@@ -35,39 +35,7 @@ function initializeTimer() {
 }
 
 
-  function showInterval() {
-    
-    let date = new Date(); /* creating object of Date class */
-    let hour = date.getHours() + hourStart;
-    let min = date.getMinutes() + minStart;
-    let sec = date.getSeconds() + secStart;
-
-    if (sec < 0) {
-        sec = sec + 60;
-        min = min - 1;
-    }
-    if (min < 0) {
-        min = min + 60;
-        hour = hour - 1;
-    }
-    if (hour < 0 ) {
-        hour = hour + 12;
-    }
-
-    sec = sec < 0 ? sec + 60 : sec;
-    min = min < 0 ? min + 60 : min;
-    hour = hour < 0 ? hour + 12 : hour;
-
-
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
- 
-    let currentTime = hour + ":" + min + ":" + sec ;
-    document.getElementById("timer").innerHTML = currentTime;
-
-}
-
+  
 
 function play() {
     let myMusic= document.getElementById("music");
@@ -105,3 +73,35 @@ function initializeTimer() {
 }
 
 
+  function showInterval() {
+    
+    let date = new Date(); /* creating object of Date class */
+    let hour = date.getHours() + hourStart;
+    let min = date.getMinutes() + minStart;
+    let sec = date.getSeconds() + secStart;
+
+    if (sec < 0) {
+        sec = sec + 60;
+        min = min - 1;
+    }
+    if (min < 0) {
+        min = min + 60;
+        hour = hour - 1;
+    }
+    if (hour < 0 ) {
+        hour = hour + 12;
+    }
+
+    sec = sec < 0 ? sec + 60 : sec;
+    min = min < 0 ? min + 60 : min;
+    hour = hour < 0 ? hour + 12 : hour;
+
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+ 
+    let currentTime = hour + ":" + min + ":" + sec ;
+    document.getElementById("timer").innerHTML = currentTime;
+
+}
